4f228c0372306497dc938b3f6a3565abaa2f6927
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-09-21 15:30:45 +0900

    検索

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,15 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = if search_param.present?
+                  Article.where('content like ?', "%#{search_param.gsub(/[\\%_]/) { |m| "\\#{m}" }}%")
+                else
+                  Article.all
+                end
+  end
+
+  private
+
+  def search_param
+    params.fetch(:q, {})
   end
 end
